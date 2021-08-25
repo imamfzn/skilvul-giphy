ab6f78147b9523d33c7e2d91e93fb856b8479f19
Muhammad Imam Fauzan <imamfzn@gmail.com>
2021-08-25 23:15:57 +0700

    Init First Page and Simple Routing to Another Pages (#2)
    * add react router
    
    * add bootstrap from cdn
    
    * init first page and simple route to another pages

---
+++ src/components/GifItem.js
@@ -0,0 +1,11 @@
+import defaultGif from '../img/iron-man-default.gif'
+
+function GifItem() {
+  return (
+    <div className="col-4">
+      <img src={defaultGif}/>
+    </div>
+  );
+}
+
+export default GifItem;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import IronManGiphy from './IronManGiphy'
+import SearchGiphy from './SearchGiphy'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App}/>
+        <Route exact path="/iron-man-giphy" component={IronManGiphy} />
+        <Route exact path="/search-giphy" component={SearchGiphy} />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
