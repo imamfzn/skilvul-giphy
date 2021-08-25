05d9bdfeabd307c15997196134db7492ec7ebff5
Muhammad Imam Fauzan <imamfzn@gmail.com>
2021-08-26 01:49:06 +0700

    init simple search page with search bar (#4)

--- src/SearchGiphy.js
+++ src/SearchGiphy.js
@@ -1,7 +1,45 @@
-function SearchGiphy() {
+import React, { useState, useEffect } from 'react';
+import GifList from './components/GifList';
+import SearchBar from './components/SearchBar';
+
+function SearchGiphy (props) {
+  const [input, setInput] = useState('');
+  const [gifList, setGifList] = useState([]);
+
+  const fetchData = async (keyword = 'coding') => {
+    return fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${keyword}&limit=9`)
+      .then(res => res.json())
+      .then(res => res.data.map(data => {
+        return {
+          id: data.id,
+          url: data.images.original.url
+        };
+      }))
+      .then(gifs => {
+        setGifList(gifs);
+      });
+  };
+
+  const updateInput = async (keyword) => {
+    setInput(keyword);
+    fetchData(keyword);
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => fetchData(), 1000);
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
-    <div>
-      <h1>Search Giphy</h1>
+    <div className="container">
+      <h1>Your Search Giphy</h1>
+
+      <SearchBar
+        keyword={input}
+        setKeyword={updateInput}
+      />
+
+      <GifList gifs={gifList} />
     </div>
   );
 }
