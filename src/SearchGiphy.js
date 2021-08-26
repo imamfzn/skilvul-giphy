ba3c2e6b0d81fd9468f4bd307da1289bb488fe44
Muhammad Imam Fauzan <imamfzn@gmail.com>
2021-08-26 23:48:12 +0700

    Make Giphy Fetcher Reuseable (#7)

--- src/SearchGiphy.js
+++ src/SearchGiphy.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import GifList from './components/GifList';
 import SearchBar from './components/SearchBar';
+import GiphyFetcher from './helper/GiphyFetcher'
 import './Page.css'
 
 function SearchGiphy (props) {
@@ -8,15 +9,9 @@ function SearchGiphy (props) {
   const [gifList, setGifList] = useState([]);
   const [typingTimeout, setTypingTimeout] = useState(0);
 
-  const fetchData = async (keyword = 'coding') => {
-    return fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${keyword}&limit=9`)
-      .then(res => res.json())
-      .then(res => res.data.map(data => {
-        return {
-          id: data.id,
-          url: data.images.fixed_height_downsampled.url
-        };
-      }))
+  const fetchData = async (keyword) => {
+    return GiphyFetcher
+      .fetch(keyword)
       .then(gifs => {
         setGifList(gifs);
       });
