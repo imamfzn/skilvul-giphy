bea28a893d419d416675aa6c9fe2157125b38f36
Muhammad Imam Fauzan <imamfzn@gmail.com>
2021-08-27 00:38:06 +0700

    Simple Loading Animation (#8)

--- src/components/GifItem.js
+++ src/components/GifItem.js
@@ -1,9 +1,22 @@
+import React, { useState } from 'react';
 import './GifItem.css';
+import Loading from '../loading.gif';
 
 function GifItem({ gif }) {
+  const [isLoaded, setLoaded] = useState(false);
+
+  const onLoadHandler = () => {
+    setLoaded(true);
+  };
+
+  const hideImgStyle = {
+    display: 'none'
+  };
+
   return (
     <div className="col-md-4 col-sm-6 gif-item">
-      <img className="gif-img" src={gif.url} alt=""/>
+      <img className="gif-loading" src={Loading} style={ isLoaded ? hideImgStyle : {} } alt=""/>
+      <img className="gif-img" src={gif.url} onLoad={onLoadHandler} style={ isLoaded ? {} : hideImgStyle } alt=""/>
     </div>
   );
 }
