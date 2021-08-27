43b5c97427841b74d42599f6b5b6d976ec53f2b9
imamfzn <imamfzn@gmail.com>
2021-08-27 17:06:18 +0700

    handle not found giphy

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,7 +1,17 @@
 import GifItem from './GifItem'
 
-function GifList({ gifs }) {
+function GifList({ gifs, firstTime = true }) {
   function renderItems() {
+    if (!firstTime && gifs.length === 0) {
+      return (
+        <div>
+          <p className="text-center">
+            Your giphy not found :(. Try to use another keywords :)
+          </p>
+        </div>
+      );
+    }
+
     return gifs.map(gif => {
       return <GifItem gif={gif} key={gif.id}/>
     });
