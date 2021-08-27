440f1f46327b0c1d3a84f92882a27a9ca9fe23cd
imamfzn <imamfzn@gmail.com>
2021-08-27 17:15:18 +0700

    add class for not found info

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -4,10 +4,8 @@ function GifList({ gifs, firstTime = true }) {
   function renderItems() {
     if (!firstTime && gifs.length === 0) {
       return (
-        <div>
-          <p className="text-center">
-            Your giphy not found :(. Try to use another keywords :)
-          </p>
+        <div className="search-result-info">
+          <p>Your giphy not found :(. Try to use another keywords</p>
         </div>
       );
     }
